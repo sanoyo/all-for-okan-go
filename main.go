f7e9ba13d580bc5914b009383752cef7a3b1f2ed
sanoyo <sa00380304yo@gmail.com>
2021-10-24 11:34:46 +0900

    Apply automigration by gorm

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/sanoyo/all-for-okan-go/src/database"
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
-	if err != nil {
-		panic("Could not connect with the database!")
-	}
+	database.Connect()
+	database.AutoMigrate()
 
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
